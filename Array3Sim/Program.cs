024d8c9797d3415c56fcdfd448207b71b47474c2
Vitaliy <wsvetkin@gmail.com>
2023-05-10 21:24:59 +0300

    добавлена блок-схема алгоритма решения задачи

--- Array3Sim/Program.cs
+++ Array3Sim/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива: "); 
+int size = Int32.Parse(Console.ReadLine());
+
+string elements;
+
+string[] firstArray = new string[size];
+
+for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите элемент масива № {i+1}");
+        firstArray[i] = Console.ReadLine();
+    }
+
+for(int i = 0; i < size; i++)
+    {
+        if(firstArray[i].Length < 3)
+        {
+            secondArray[i] = firstArray[i];
+        }
+    }
+
+Console.WriteLine(String.Join(" ", firstArray));
